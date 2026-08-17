ee6f27fd040521350a6eea3af22a33c91cb48e59
DanahAlsahli <156077197+DanahAlsahli@users.noreply.github.com>
2026-08-17 02:50:01 -0800

    Add Q6

--- src/Main.java
+++ src/Main.java
@@ -64,33 +64,55 @@ public class Main {
         System.out.println(Arrays.toString(x));
 
         // Q5:
-        System.out.println("how many numbers do u want?");
-        int p = input.nextInt();
-
-        int[] pp = new int[p];
-
-        for (int i = 0; i < pp.length; i++) {
-            System.out.println("enter a number:");
-            pp[i] = input.nextInt();
+        System.out.println("How many numbers?");
+        int n5= input.nextInt();
+        int[] num5= new int[n5];
+        for (int i=0; i<num5.length; i++) {
+            System.out.println("Enter number:");
+            num5[i]= input.nextInt();
         }
 
-        int[] result = new int[p];
-        int ind = 0;
-
-        for (int i = 0; i < pp.length; i++) {
-            if (pp[i] % 2 != 0) {
-                result[ind] = pp[i];
-                ind++;
+        int[] result=new int[n5];
+        int index= 0;
+        for (int i=0; i<num5.length; i++) {
+            if (num5[i] % 2 !=0) {
+                result[index]= num5[i];
+                index++;
             }
         }
 
-        for (int i = 0; i < pp.length; i++) {
-            if (pp[i] % 2 == 0) {
-                result[ind] = pp[i];
-                ind++;
+        for (int i=0; i<num5.length; i++) {
+            if (num5[i] % 2==0) {
+                result[index]= num5[i];
+                index++;
             }
         }
+        System.out.println("New array: " + Arrays.toString(result));
 
-        System.out.println("new array: " + Arrays.toString(result));
+        // Q6:
+        System.out.println("How many numbers?");
+        int n6=input.nextInt();
+
+        int[] num1= new int[n6];
+        int[] num2= new int[n6];
+
+        System.out.println("Enter first array:");
+
+        for (int i=0; i<num1.length; i++) {
+            num1[i]= input.nextInt();
+        }
+
+        System.out.println("Enter second array:");
+
+        for (int i=0; i<num2.length; i++) {
+            num2[i]= input.nextInt();
+        }
+
+        if (Arrays.equals(num1, num2)) {
+            System.out.println("true");
+        } else {
+            System.out.println("false");
+        }
+
     }
 }
